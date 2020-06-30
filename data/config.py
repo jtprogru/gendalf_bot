b6d568496cdb76302f59534265ffb700dbebac19
Mihael Savin <jtprogru@gmail.com>
2020-06-30 16:25:04 +0300

    Fix config.py

--- data/config.py
+++ data/config.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
-admins = [
-]
+MASTER_ADMIN_ID = str(os.getenv("MASTER_ADMIN_ID"))
+ADMIN_LIST = [int(MASTER_ADMIN_ID), ]
+LINK_CHAT_RULES = str(os.getenv("LINK_CHAT_RULES"))
 
-ip = os.getenv("ip")
+ip = os.getenv("REDIS_IP")
 
 aiogram_redis = {
     'host': ip,
