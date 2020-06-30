31580a7c3c3961330a3e89dacb84117b8040e096
Mihael Savin <jtprogru@gmail.com>
2020-07-01 01:26:21 +0300

    Add basic Unsplash configuration

--- data/config.py
+++ data/config.py
@@ -10,6 +10,10 @@ ADMIN_LIST = [int(MASTER_ADMIN_ID), ]
 LINK_CHAT_RULES = str(os.getenv("LINK_CHAT_RULES"))
 
 ip = os.getenv("REDIS_IP")
+# Unsplash API key
+UNSPLASH_API_KEY = os.getenv("UNSPLASH_API_KEY")
+TMP_DIR = os.getenv("TMP_PATH")
+IMG_DIR = os.getenv("IMG_PATH")
 
 aiogram_redis = {
     'host': ip,
