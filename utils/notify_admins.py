5c48900384af9db4f678e152725bc6b95eab97b6
Mihael Savin <jtprogru@gmail.com>
2020-06-30 16:26:31 +0300

    Fix admin notify fucntion;

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,13 +2,16 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.config import admins
+from data.config import ADMIN_LIST
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in admins:
+    for admin in ADMIN_LIST:
         try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+            await dp.bot.send_message(
+                chat_id=int(admin),
+                text="Бот Запущен"
+            )
 
         except Exception as err:
             logging.exception(err)
