918aef8157145c7beb8afcb59e1687b943689a33
JTProg <mail@jtprog.ru>
2020-08-06 19:51:35 +0300

    Add Sentry;

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -3,11 +3,9 @@ import sentry_sdk
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from data.config import SENTRY_DSN
 
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    integrations=[AioHttpIntegration()]
+sentry_sdk.init(dsn=SENTRY_DSN, integrations=[AioHttpIntegration()])
+logging.basicConfig(
+    format=u"%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s",
+    # level=logging.INFO,
+    level=logging.DEBUG,
 )
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    # level=logging.INFO,
-                    level=logging.DEBUG,
-                    )
