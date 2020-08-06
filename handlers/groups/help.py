b22ae2b03c9c22d9f4287c3a0c56053c42673696
JTProg <mail@jtprog.ru>
2020-08-06 19:57:16 +0300

    Help command;

--- handlers/groups/help.py
+++ handlers/groups/help.py
@@ -7,28 +7,28 @@ from utils.misc import rate_limit
 from data.config import ADMIN_LIST
 
 
-@rate_limit(55, 'help')
+@rate_limit(55, "help")
 @dp.message_handler(CommandHelp())
 async def user_bot_help(message: types.Message):
     if message.from_user.id in ADMIN_LIST:
         text = [
-            'Список команд: ',
-            '/start - Начать диалог',
-            '/help - Получить справку',
-            '/ban - Забанить',
-            '/unban - Разбанить',
-            '/ping - Тест бота',
-            '/report - Пожаловаться на мудака',
-            '/pics - Получить картинку',
+            "Список команд: ",
+            "/start - Начать диалог",
+            "/help - Получить справку",
+            "/ban - Забанить",
+            "/unban - Разбанить",
+            "/ping - Тест бота",
+            "/report - Пожаловаться на мудака",
+            "/pics - Получить картинку",
         ]
-        await message.reply('\n'.join(text))
+        await message.reply("\n".join(text))
     else:
         text = [
-            'Список команд: ',
-            '/start - Начать диалог',
-            '/help - Получить справку',
-            '/ping - Тест бота',
-            '/report - Пожаловаться на мудака',
-            '/pics - Получить картинку',
+            "Список команд: ",
+            "/start - Начать диалог",
+            "/help - Получить справку",
+            "/ping - Тест бота",
+            "/report - Пожаловаться на мудака",
+            "/pics - Получить картинку",
         ]
-        await message.reply('\n'.join(text))
+        await message.reply("\n".join(text))
