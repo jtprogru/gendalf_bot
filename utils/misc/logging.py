33fa83393386580ab4b6093ef910a85c098155de
Mihael Savin <jtprog@users.noreply.github.com>
2020-08-07 23:31:35 +0300

    Update logging.py
    Fix logging level;

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -6,6 +6,5 @@ from data.config import SENTRY_DSN
 sentry_sdk.init(dsn=SENTRY_DSN, integrations=[AioHttpIntegration()])
 logging.basicConfig(
     format=u"%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s",
-    # level=logging.INFO,
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
